78b43b0c50b33f1cabdb39c045c2e0d19f5b8130
xGIANOx <studioenricogian@gmail.com>
2023-04-11 16:07:32 +0200

    [add] rando generator of 10 email addresses

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,24 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        random_mail_url: 'https://flynn.boolean.careers/exercises/api/random/mail',
+        my_data: null,
+        random_mail: [],
+
       }
+    },
+    mounted(){
+       
+            for (let index = 0; index < 10; index++) {
+                axios
+                .get(this.random_mail_url)
+                .then(response => {
+                console.log(response.data.response);
+                this.my_data = response.data
+                this.random_mail.push(response.data.response)
+            });
+                
+            }
+        
     }
   }).mount('#app')
