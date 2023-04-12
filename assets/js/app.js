f9341b011a458041c345e55bfe278f67b2cc1974
xGIANOx <studioenricogian@gmail.com>
2023-04-12 09:58:33 +0200

    [fix] exercise

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,13 +5,12 @@ const { createApp } = Vue
       return {
         random_mail_url: 'https://flynn.boolean.careers/exercises/api/random/mail',
         random_mail: [],
-        emails_to_generate: 10,
 
       }
     },
     mounted(){
        
-            for (let index = 0; index < this.emails_to_generate; index++) {
+            for (let index = 0; index < 10; index++) {
                 axios
                 .get(this.random_mail_url)
                 .then(response => {
